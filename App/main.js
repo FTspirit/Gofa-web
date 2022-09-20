0f25ff3e416ff682c7838956b19261728d541aec
FTspirit <90292792+FTspirit@users.noreply.github.com>
2022-09-21 01:06:32 +0700

    first commit

---
+++ App/main.js
@@ -0,0 +1,49 @@
+"use strict";
+const btnNavEL = document.querySelector(".btn-mobile-nav");
+const navEL = document.querySelector(".nav");
+btnNavEL.addEventListener("click", function () {
+  navEL.classList.toggle("nav-open");
+});
+/////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+//////////////////////////////////////
+// Auto slide text
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3500); // Change image every 3.5 seconds
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex - 1].className += " active";
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# Gofa-web
+# Tóm tắt
+- GOFA - Giải pháp dẫn đường kết hợp cảnh báo tốc độ miễn phí
+  Gồm 6 tính năng nổi bật:
+    + Bản đồ
+    + Tìm kiếm địa chỉ
+    + Tìm kiếm đường đi
+    + Dẫn đường
+    + Cảnh báo tốc độ
+    + Chia sẻ bạn bè
+
+# TỐI ƯU
+- Hãy cài đặt IOS 14 trở lên để web được hoạt động tối ưu nhất
+
+# CÔNG NGHỆ
+- Gofa-web được viết bằng html5,Css3,Javascript và thư viện Ionicons, Fontawesome
+
+# HƯỚNG DẪN SỬ DỤNG
+- Click download on the Applestore để tải app trên cửa hàng
+- Quét mã QR để tải app trên cửa hàng
+
+  
+  
